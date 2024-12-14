ee44dd029663c7f8721506170f5cebf39a0e6bd9
Tolga YILDIZ <tolgayildiz91@hotmail.com.tr>
2024-12-14 21:52:28 +0300

    Identityserver init

--- Services/Order/Infrastructure/MultiShop.Order.Persistence/Context/OrderContext.cs
+++ Services/Order/Infrastructure/MultiShop.Order.Persistence/Context/OrderContext.cs
@@ -12,7 +12,7 @@ namespace MultiShop.Order.Persistence.Context
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("server=.;initial Catalog=MultiShopOrderDb;User=sa;Password=123456aA*");
+            optionsBuilder.UseSqlServer("server=localhost,1440;initial Catalog=MultiShopOrderDb;User=sa;Password=123456aA*");
         }
         public DbSet<Address> Addresses { get; set; }
         public DbSet<OrderDetail> OrderDetails { get; set; }
